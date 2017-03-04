156257df819e5c99e378740c93969bab21077f4d
Adrian Booth <adrian.booth17@gmail.com>
2017-03-04 22:28:20 +0000

    refactor parsing logic to separate class

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,12 +1,11 @@
 require 'pry'
+require './lib/sudoku_parser'
+
 class Validator
 
   INVALID = "This sudoku is invalid."
   VALID_BUT_INCOMPLETE = "This sudoku is valid, but incomplete."
   VALID = "This sudoku is valid."
-
-  ROW_SEPERATOR = "-"
-  COLUMN_SEPERATOR = "|"
 
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
@@ -19,44 +18,41 @@ class Validator
   end
 
   def validate
-    validate_rows
-    validate_columns
-    validate_blocks
+    scan_rows
+    scan_colunms
+    scan_blocks
+    return_message
+  end
+
+  private
+
+  def return_message
     return INVALID if @invalid != 0
-
-    if @valid_but_incomplete != 0
-      return VALID_BUT_INCOMPLETE
-    else
-      return VALID
-    end
+    return VALID_BUT_INCOMPLETE if @valid_but_incomplete != 0
+    return VALID
   end
 
-  def validate_rows
-    grid_blocks.map { |section| check_for_validity (section) }
+  def scan_rows
+    parsed_blocks.map { |section| check_for_validity (section) }
   end
 
-  def validate_columns
-    @columns = grid_blocks.flatten.each_slice(9).to_a.transpose
+  def scan_colunms
+    @columns = parsed_blocks.flatten.each_slice(9).to_a.transpose
     check_for_validity(@columns)
   end
 
-  def validate_blocks
-    @groups = grid_blocks.flatten(1).transpose.flatten(1).each_slice(3).to_a
+  def scan_blocks
+    @groups = parsed_blocks.flatten(1).transpose.flatten(1).each_slice(3).to_a
     check_for_validity(@groups)
   end
 
-  def grid_blocks
-    @blocks = @puzzle_string.split("\n").reject {|row| row.include?(ROW_SEPERATOR)}.each_slice(3).to_a.map! do |row|
-      row.map! do |string|
-        string.split(COLUMN_SEPERATOR).map! do |str|
-          str.split(" ").map(&:to_i)
-        end
-      end
-    end
+  def parsed_blocks
+    SudokuParser.parse(@puzzle_string)
   end
 
   def check_for_validity(blocks)
     blocks.map do |block|
+      remove_zeros(block)
       if block_is_invalid?(block)
          @invalid +=1
       elsif block_is_valid_but_incomplete?(block)
@@ -67,16 +63,18 @@ class Validator
 
   def block_is_invalid?(block)
     if block.flatten.count(0) >= 1
-      no_zeros = block.flatten.reject {|num| num == 0 }
-      no_zeros.flatten.length != no_zeros.flatten.uniq.length
+      @no_zeros.flatten.length != @no_zeros.flatten.uniq.length
     else
       (block.flatten.length != block.flatten.uniq.length || block.flatten.sort != [*1..9])
     end
   end
 
   def block_is_valid_but_incomplete?(block)
-    no_zeros = block.flatten.reject {|num| num == 0}
-    (no_zeros.length == no_zeros.uniq.length) && block.flatten.count(0) > 0
+    (@no_zeros.length == @no_zeros.uniq.length) && block.flatten.count(0) > 0
+  end
+
+  def remove_zeros(block)
+    @no_zeros = block.flatten.reject {|num| num == 0}
   end
 
 end
